0c694be90c2b5c1d446935fd9fc0f99c972499e4
kheina <vixen@kheina.com>
2021-06-21 14:22:17 -0400

    add response examples

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from kh_common.server import JsonResponse, NoContentResponse, Request, ServerApp
 from kh_common.caching import KwargsCache
 from kh_common.models.auth import Scope
-from fastapi.responses import Response
+from kh_common.models.user import User
 from models import SetMod, UpdateSelf
+from typing import List
 from users import Users
 
 
@@ -15,7 +16,7 @@ async def shutdown() :
 	users.close()
 
 
-@app.get('/v1/fetch_user/{handle}')
+@app.get('/v1/fetch_user/{handle}', responses={ 200: { 'models': User } })
 @KwargsCache(60)
 async def v1FetchUser(handle: str) :
 	return JsonResponse(
@@ -23,8 +24,8 @@ async def v1FetchUser(handle: str) :
 	)
 
 
-@app.get('/v1/fetch_self')
-async def v1FetchSelf(req: Request) :
+@app.get('/v1/fetch_self', responses={ 200: { 'models': User } })
+async def v1FetchSelf(req: Request) -> User :
 	await req.user.authenticated()
 
 	return JsonResponse(
@@ -32,8 +33,8 @@ async def v1FetchSelf(req: Request) :
 	)
 
 
-@app.post('/v1/update_self')
-async def v1UpdateSelf(req: Request, body: UpdateSelf) :
+@app.post('/v1/update_self', responses={ 204: { 'models': None } })
+async def v1UpdateSelf(req: Request, body: UpdateSelf) -> None :
 	await req.user.authenticated()
 
 	users.updateSelf(
@@ -48,16 +49,16 @@ async def v1UpdateSelf(req: Request, body: UpdateSelf) :
 	return NoContentResponse
 
 
-@app.get('/v1/all_users')
-async def v1FetchUsers(req: Request) :
+@app.get('/v1/all_users', responses={ 200: { 'models': List[User] } })
+async def v1FetchUsers(req: Request) -> List[User] :
 	await req.user.verify_scope(Scope.admin)
 	return JsonResponse(
 		users.getUsers()
 	)
 
 
-@app.post('/v1/set_mod')
-async def v1SetMod(req: Request, body: SetMod) :
+@app.post('/v1/set_mod', responses={ 204: { 'models': None } })
+async def v1SetMod(req: Request, body: SetMod) -> None :
 	await req.user.verify_scope(Scope.admin)
 	users.setMod(body.handle, body.mod)
 	return NoContentResponse
